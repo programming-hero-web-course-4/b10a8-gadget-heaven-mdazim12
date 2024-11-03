e474933984a21a922dc45118fb5e03af30fcc8c3
Azim <azimuddin807@gmail.com>
2024-11-03 21:50:05 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,40 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer section</h3>
-        </div>
+
+       
+
+        <footer className=" bg-white text-black p-10">
+
+           
+                <h2 className="text-black text-3xl font-bold text-center mt-10">Gadget Haven</h2>
+                <p className="text-base font-medium text-center py-5 mb-10 text-gray-600">Leading the way in cutting-edge technology and innovation.</p>
+            
+                <div className="divider"></div>
+
+            <div className="footer text-black font-medium">
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
+import { Link, NavLink } from "react-router-dom";
+import { CiHeart } from "react-icons/ci";
+import { BsCart } from "react-icons/bs";
+
+
+
+
 
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-[#9538E2] ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -22,36 +29,35 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        
+
+                        <NavLink to = '/'>Home</NavLink>
+                        
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl text-white">
+                    <Link to = '/'>Have Gadget</Link>
+                </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                        
+                       <li> <NavLink to = '/'>Home</NavLink></li>
+                       
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+           
+           <span className="bg-white p-2 text-lg rounded-full m-2">
+                 <BsCart />
+           </span>
+
+           <span className="bg-white p-2 text-lg rounded-full m-2">
+                <CiHeart />
+           </span>
+            
+                
+
             </div>
         </div>
     );
